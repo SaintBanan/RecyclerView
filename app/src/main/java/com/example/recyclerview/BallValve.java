4436a75eb553f16e5f2e759b4509dc7b18683305
SaintBanan <gumnew@mail.ru>
2020-12-08 14:48:31 +0700

    В данной работе я решил вместо стандартного примера сделать список, состоящий из шаровых кранов и их некоторых характеристик. Т.о., вместо DummyContent и DummyItem добавлены классы BallValve и BallValveContent

---
+++ app/src/main/java/com/example/recyclerview/BallValve.java
@@ -0,0 +1,36 @@
+package com.example.recyclerview;
+
+//Шаровой кран
+public class BallValve {
+    private String name, manufacturer, material, lever;
+    private static int count;
+    private int id;
+
+    static {
+        count = 0;
+    }
+
+    BallValve(String name, String manufacturer, String material, String lever) {
+        count++;
+        this.manufacturer = manufacturer;   //Производитель
+        this.material = material;           //Материал
+        this.lever = lever;                 //Тип рукоятки
+        this.name = name;
+        id = count;
+    }
+
+    //Получить наименование товара
+    public String getName() {
+        return name;
+    }
+
+    //Получить id товара
+    public int getId() {
+        return id;
+    }
+
+    //Получить характеристики товара в виде строки
+    public String getFeatures() {
+        return String.format("Характеристики:\n Производитель: %s\n Материал: %s\n Рукоятка: %s", manufacturer, material, lever);
+    }
+}
---
+++ app/src/main/java/com/example/recyclerview/BallValveContent.java
@@ -0,0 +1,19 @@
+package com.example.recyclerview;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BallValveContent {
+    public static final List<BallValve> ball_valves = new ArrayList<BallValve>();
+
+    static {
+        String[] names = {"Royal Thermo Optimal", "Кран шаровой Giacomini", "Кран шаровой газовый Giacomini"};
+        String[] manufacturers = {"Royal Thermo", "Giacomini", "Giacomini"};
+        String[] materials = {"Латунь", "Латунь", "Латунь"};
+        String[] levers = {"Бабочка", "Бабочка", "Рычаг"};
+
+        for (int i = 0; i < 3; i++) {
+            ball_valves.add(new BallValve(names[i], manufacturers[i], materials[i], levers[i]));
+        }
+    }
+}
